7f4dabdfc028b279e6edcf0e35cb8c6c86502e94
Alex Clemmer <clemmer.alexander@gmail.com>
2013-06-18 12:03:49 -0400

    Add midje as dev dependency

--- project.clj
+++ project.clj
@@ -5,8 +5,10 @@
   :url "http://www.regret.io"
   :license {:name "MIT"
             :url "http://github.com/hausdorff/regret/blob/master/LICENSE"}
+  ;:dev-dependencies [[midje "1.5.1"]]
   :dependencies [[org.apache.mahout/mahout-core "0.7"]
                  [org.clojure/clojure "1.4.0"]
                  [org.clojure/core.match "0.2.0-alpha12"]
                  [storm/storm-lib "0.8.2"]]
-  :main regret.core)
+  :main regret.core
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
